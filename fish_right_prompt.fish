10a13e27dee70388b45d5a153bd47e4af6bfb43b
chris-vest <hotdogsandfrenchfries@gmail.com>
2020-05-22 10:11:54 +0200

    Fix status_indicator

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,16 +1,14 @@
 function fish_right_prompt
+  set -l last_status $status
+
   set -l green (set_color -o green)
   set -l red (set_color -o red)
 
-  set -l last_status $status
-
-  if test $last_status = 0
-      set status_indicator "$green✔︎ "
+  if not test $last_status = 0
+    set status_indicator "$red✗ "
   else
-      set status_indicator "$red✗ "
+    set status_indicator "$green✔︎ "
   end
-
-  set -l last_status $status
 
   echo -n -s $status_indicator 
 end
