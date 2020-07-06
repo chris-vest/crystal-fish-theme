89f5b506c87c683e51dcdc5e0c186caf9f413c94
chris-vest <intellivision@pm.me>
2020-07-06 11:26:57 +0200

    Use Dracula Vim color scheme

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,8 +1,8 @@
 function fish_right_prompt
   set -l last_status $status
 
-  set -l green (set_color -o green)
-  set -l red (set_color -o red)
+  set -l green (set_color -o 50FA7B)
+  set -l red (set_color -o FF5555)
 
   if not test $last_status = 0
     set status_indicator "$red✗ "
@@ -10,5 +10,5 @@ function fish_right_prompt
     set status_indicator "$green✔︎ "
   end
 
-  echo -n -s $status_indicator 
+  echo -n -s $status_indicator
 end
